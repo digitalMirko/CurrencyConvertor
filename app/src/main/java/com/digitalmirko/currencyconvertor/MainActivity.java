104507e99626c5eb57390bfcb0b2f95185b3d978
Mirko Cukich <mirkocukich@gmail.com>
2016-12-21 17:44:23 -0600

    Program Complete

--- app/src/main/java/com/digitalmirko/currencyconvertor/MainActivity.java
+++ app/src/main/java/com/digitalmirko/currencyconvertor/MainActivity.java
@@ -6,8 +6,6 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import org.w3c.dom.Text;
-
 public class MainActivity extends AppCompatActivity {
 
     public void btnClicked(View view){
