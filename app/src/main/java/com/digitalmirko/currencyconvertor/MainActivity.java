290a348d7008946122b88ad3c20eb35f1aa50504
Mirko Cukich <mirkocukich@gmail.com>
2016-12-21 14:18:42 -0600

    Btn to Toast test

--- app/src/main/java/com/digitalmirko/currencyconvertor/MainActivity.java
+++ app/src/main/java/com/digitalmirko/currencyconvertor/MainActivity.java
@@ -2,8 +2,20 @@ package com.digitalmirko.currencyconvertor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void btnClicked(View view){
+
+        EditText dollarsTextEdit = (EditText) findViewById(R.id.dollarsTextEdit);
+
+
+        // testing typed in entry to Toast Message
+        Toast.makeText(MainActivity.this, dollarsTextEdit.getText().toString(), Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
